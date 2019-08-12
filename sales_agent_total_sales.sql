dbe5ad59c62db94da21617c61ae4dca23a63bbf2
Carly Bergthold <46655107+carlybergthold@users.noreply.github.com>
2019-08-12 08:03:55 -0700

    added subquery

--- sales_agent_total_sales.sql
+++ sales_agent_total_sales.sql
@@ -1,5 +1,5 @@
-SELECT e.EmployeeId AS SalesRep, SUM(i.Total) AS TotalSales
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS SalesRep, SUM(i.Total) AS TotalSales
 FROM Employee e
 JOIN Customer c ON c.SupportRepId = e.EmployeeId
 JOIN Invoice i ON i.CustomerId = c.CustomerId
-GROUP BY e.EmployeeId
+GROUP BY e.EmployeeId, e.FirstName, e.LastName;
--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,7 +1,8 @@
-SELECT TOP 1 (e.FirstName + ' ' + e.LastName) AS SalesRep, SUM(i.Total) AS TotalSales
+SELECT MAX(SalesRep) AS SalesRep, MAX(TotalSales) AS TotalSales
+FROM
+(SELECT CONCAT(e.FirstName, ' ', e.LastName) AS SalesRep, SUM(i.Total) AS TotalSales
 FROM Employee e
 JOIN Customer c ON c.SupportRepId = e.EmployeeId
 JOIN Invoice i ON i.CustomerId = c.CustomerId
 WHERE Year(InvoiceDate) = 2009
-GROUP BY (e.FirstName + ' ' + e.LastName)
-ORDER BY TotalSales DESC
+GROUP BY e.EmployeeId, e.FirstName, e.LastName) AS SalesRep
